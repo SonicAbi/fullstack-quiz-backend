91a3368ff3b5a4ea35070538ecf19ae5effb8838
Mehmet Erdem Koc <mehmet.erdem.koc@volkswagen.de>
2025-04-03 09:01:54 +0200

    add initial category seed data for database

---
+++ src/db/seed.ts
@@ -0,0 +1,9 @@
+import type { NewCategory } from "./schemas";
+
+const categories: NewCategory[] = [
+  { name: "API" },
+  { name: "HTML & CSS" },
+  { name: "React" },
+  { name: "TypeScript" },
+  { name: "JavaScript" },
+];

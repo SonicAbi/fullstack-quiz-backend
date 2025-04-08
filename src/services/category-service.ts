3d333dcec2f8953bd9f84b396477ad35ae06818b
Mehmet Erdem Koc <mehmet.erdem.koc@volkswagen.de>
2025-04-08 15:19:33 +0200

    refactor: format getOneCategory method for improved readability

--- src/services/category-service.ts
+++ src/services/category-service.ts
@@ -5,11 +5,11 @@ import { CategoryRepository } from "../repositories/category-repository";
 export const CategoryService = {
   async getAllCategories(): Promise<Category[]> {
     return CategoryRepository().findMany();
-    },
-    
-    async getOneCategory(category?: string) {
-        let condition = undefined
-        if (category) condition = eq(categoriesTable.name, category)
-        return CategoryRepository().findOne(condition)
-    }
+  },
+
+  async getOneCategory(category?: string) {
+    let condition = undefined;
+    if (category) condition = eq(categoriesTable.name, category);
+    return CategoryRepository().findOne(condition);
+  },
 };
